744885cfc02c8701adc01557baf4c654303350f3
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 13:18:45 +0100

    Update L2_DataTypes.js

--- L2_DataTypes.js
+++ L2_DataTypes.js
@@ -1,5 +1,5 @@
 
 let x = 5; // number
-let y = 3.14; // number
-let name = "Justyna"; // String
-let isVlid = true; // boolean
+const y = 3.14; // number const for variables which cant be reasigned
+var name = "Justyna"; // String 
+let isVlid = true; // boolean let when variable when can be reassigned somewhere else
