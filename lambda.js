05df1696e4e98ac9b7d3bb0f27b0bab374605eed
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 13:24:24 +0100

    Create lambda.js

---
+++ lambda.js
@@ -0,0 +1,11 @@
+
+exports.handler = async (event) => {
+    console.log("Received event:", JSON.stringify(event, null, 2));
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify("Hello from Lambda!"),
+    };
+
+    return response;
+}
