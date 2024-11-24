2152923cc1a6d32ffe696a5f61b2876c179cbec7
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 16:56:02 +0100

    Update lesson_6_for_loops.js

--- lesson_6_for_loops.js
+++ lesson_6_for_loops.js
@@ -3,8 +3,22 @@
 
 let regions = ["us-west-1", "us-east-1", "eu-west-1", "ap-southeast-1"];
 
-for (let 1 = 0; i < regions.length; i++) {
+// 1. let i = 0; initialization step, runs at the beginning of the loop, it declares variable named i and sets its initial value to 0
+// 2. i < regions.length; second part which is condition, loop condition which checks before every iteration of the loop, regions.lenght gives as a number of 
+// elements in the regions array, the loop continues as long as i = 0 is less than a lenght of array, when i becomes greater or equal than region.lenght which 
+// is 4 loop will stop.
+// 3. i++ this is itteration experssion, it runs at the end of each loop itteration, i++ is a shortcut of i = i + 1, so in here it increases value of i = 0 by 1 
+
+for (let i = 0; i < regions.length; i++) {
     console.log("Deploying to region: " + regions[i]);
 }
 
 // ---- Exercise ----
+
+// Array of cloud services
+
+let cloudServices = ["EC2", "S3", "Lambda", "DynamoDB"];
+
+for (let i = 0; i < cloudServices.length; i++) {
+    console.log("Configuring AWS " + cloudServices[i] + " Service");
+}
