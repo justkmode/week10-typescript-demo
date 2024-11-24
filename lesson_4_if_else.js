70bbe0f8566386050874e521c1208b90025ebb57
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 16:27:51 +0100

    Create lesson_4_if_else.js

---
+++ lesson_4_if_else.js
@@ -0,0 +1,14 @@
+
+
+// ---- If / Else Statements
+
+let cloudProvider = "AWS";
+
+if (cloudProvider === "AWS") {
+    console.log("We're using Amazon Web Services");
+} else {
+    console.log("We're not using AWS");
+}
+
+// Exercise - Make your own If / Else Statement for Terraform
+
