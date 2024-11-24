a0ec23785ccd1330c9f06d6e747602f1eee72718
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 13:34:48 +0100

    Update lesson_2_variables.js

--- lesson_2_variables.js
+++ lesson_2_variables.js
@@ -4,7 +4,7 @@ function deployResources(isProduction) {
     let region = "eu-west-1"; // Default region for development
 
     if (isProduction) {
-        let region = "eu-west-2"; // Region for production'
+        let region = "eu-west-2"; // Region for production
         console.log("Deploying production resources in ${region}");
         // Deploy production-specific resources here
     }
