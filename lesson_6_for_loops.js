3ac5de420107f9dea9814cb69fc9636bcbc3c2ce
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 16:34:38 +0100

    Create lesson_6_for_loops.js

---
+++ lesson_6_for_loops.js
@@ -0,0 +1,10 @@
+
+// ---- For Loops ----
+
+let regions = ["us-west-1", "us-east-1", "eu-west-1", "ap-southeast-1"];
+
+for (let 1 = 0; i < regions.length; i++) {
+    console.log("Deploying to region: " + regions[i]);
+}
+
+// ---- Exercise ----
