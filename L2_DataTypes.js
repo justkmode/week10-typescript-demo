42a83c3573294ff612aa6e0a9428da65c1a6c367
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 13:31:57 +0100

    Update L2_DataTypes.js

--- L2_DataTypes.js
+++ L2_DataTypes.js
@@ -1,5 +1,5 @@
 
 let x = 5; // number
 const y = 3.14; // number const for variables which cant be reasigned
-var name = "Justyna"; // String 
+var name = "Justyna"; // String var is old and not used anymore
 let isValid = true; // boolean let when variable when can be reassigned somewhere else
