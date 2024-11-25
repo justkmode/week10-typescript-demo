4f8e45981af631091f8d66c179ae85d5252bec8a
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-25 19:19:36 +0100

    Create lesson_8_function.js

---
+++ lesson_8_function.js
@@ -0,0 +1,18 @@
+
+// Calling function
+sayHello();
+
+
+// ---- Function Parameters ----
+
+function deployToRegion(region) {
+    console.log("Deploying to region: " + region);
+}
+
+// Calling Function with Parameters
+deployToRegion("us-west-2");
+
+// // ---- Exercise ----
+// Function name configureService
+
+// 
